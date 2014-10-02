29794b3ea459bee74170512b65881f8ef1ed2c03
Andrew Niemantsverdriet <andrewniemants@gmail.com>
2014-10-02 08:49:13 -0600

    More clear names

--- application/config/sendgrid_config.php
+++ application/config/sendgrid_config.php
@@ -1,10 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config["application_title"] = 'Your Title Here';
+$config["application_title"] = 'Your Title';
 
-$config["support_email"] = 'support@example.com';
+$config["support_email"] = 'support@example';
 $config["default_send_from"] = 'noreply@example.com';
 
-$config["sendgrid_user"] = 'user';
+$config["sendgrid_user"] = 'sendgrid';
 $config["sendgrid_key"] = 'password';
 ?>
